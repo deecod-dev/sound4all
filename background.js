35a76fe0e52dd2cb92c5c3beb8d328525bac5aea
deecod-dev <devutkarsh23@gmail.com>
2024-08-06 18:50:23 +0530

    this version will work for all applications

--- background.js
+++ background.js
@@ -1,33 +1,59 @@
-let tabs = {};
-let tabval = {};
-
 // Handle messages from popup
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log("Message received:", message);
+  console.log("mess recv");
   let ops = {
-    "idk-man": async (msg) => {
-      if (msg.tabId in tabval) {
-        console.log("Volume for tabId:", tabval[msg.tabId]);
-        sendResponse(tabval[msg.tabId]);
+    "idk-man": (msg) => {
+      if (msg.tabId in tabs) {
+        console.log("Volume for tabId:", tabs[msg.tabId].gainNode.gain.value);
+        sendResponse(tabs[msg.tabId].gainNode.gain.value);
       } else {
-        sendResponse(null);
+        sendResponse(null); // Handle the case where tabId is not found
       }
     },
     "get-tab-volume": (msg) => {
-      let volume = getTabVolume(msg.tabId);
-      console.log("Volume got:", volume);
-      sendResponse(volume);
+      res(getTabVolume(msg.tabId));
+      console.log("vol got: ", getTabVolume(msg.tabId));
     },
-    "set-tab-volume": async (msg) => {
-      console.log("Setting volume");
-      await setTabVolume(msg.tabId, msg.newVol);
-      sendResponse(false);
+    "set-tab-volume": (msg) => {
+      console.log("in set vol");
+      if (msg.mute) {
+        console.log("inmute");
+        // If you want to mute
+        tabs[msg.tabId].mute = true;
+        setTabVolume(msg.tabId, 0);
+        res(true);
+      } else {
+        console.log("other case");
+        // Else set new volume
+        setTabVolume(msg.tabId, msg.newVol);
+        res(false);
+      }
     },
-    undefined: (msg) => {
-      return sendResponse(new Error("[ERR] function not implemented"));
+    "toggle-mute": async (msg) => {
+      console.log("in toggle mute");
+      if (msg.tabId in tabs) {
+        if (tabs[msg.tabId].mute) {
+          tabs[msg.tabId].mute = false;
+          await setTabVolume(msg.tabId, tabs[msg.tabId].previousVolume || 1);
+        } else {
+          tabs[msg.tabId].previousVolume = tabs[msg.tabId].gainNode.gain.value;
+          tabs[msg.tabId].mute = true;
+          await setTabVolume(msg.tabId, 0);
+        }
+        sendResponse({
+          muted: tabs[msg.tabId].mute,
+          volume: tabs[msg.tabId].gainNode.gain.value,
+        });
+      } else {
+        sendResponse(new Error("[ERR] tab not found"));
+      }
+    },
+    undefined(msg) {
+      return res(new Error("[ERR] function not implemented"));
     },
   };
 
+  console.log("msg: ", message);
   if (ops[message.name]) {
     ops[message.name](message);
   } else {
@@ -40,83 +66,95 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 // Clean everything up once the tab is closed
 chrome.tabs.onRemoved.addListener(disposeTab);
 
+let tabs = {};
+
 /**
- * Injects the content script and sets the volume.
+ * Captures a tab's sound, allowing it to be programmatically modified.
+ * We only need to call this function if the tab isn't yet in that object.
  * @param tabId Tab ID
- * @param vol Volume
  */
-async function setTabVolume(tabId, vol) {
-  console.log("Set volume");
-
-  if (!(tabId in tabs)) {
-    await injectContentScript(tabId);
-  }
-
-  chrome.tabs.sendMessage(
-    tabId,
-    { action: "setVolume", volume: vol },
-    (response) => {
+function captureTab(tabId) {
+  return new Promise((resolve, reject) => {
+    chrome.tabCapture.capture({ audio: true, video: false }, (stream) => {
       if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError);
+        reject(chrome.runtime.lastError);
         return;
       }
-      if (response && response.success) {
-        tabval[tabId] = vol;
-        updateBadge(tabId, vol);
-      }
-    }
-  );
-}
 
-/**
- * Injects the content script into the specified tab.
- * @param tabId Tab ID
- */
-function injectContentScript(tabId) {
-  return new Promise((resolve, reject) => {
-    chrome.scripting.executeScript(
-      {
-        target: { tabId },
-        files: ["content_script.js"],
-      },
-      () => {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError);
-        } else {
-          resolve();
-        }
-      }
-    );
+      const audioContext = new AudioContext();
+      const streamSource = audioContext.createMediaStreamSource(stream);
+      const gainNode = audioContext.createGain();
+
+      streamSource.connect(gainNode);
+      gainNode.connect(audioContext.destination);
+
+      tabs[tabId] = {
+        audioContext,
+        streamSource,
+        gainNode,
+        mute: false,
+        previousVolume: 1,
+      };
+      resolve();
+    });
   });
 }
 
 /**
- * Returns a tab's volume.
+ * Returns a tab's volume, `1` if the tab isn't captured yet.
  * @param tabId Tab ID
  */
 function getTabVolume(tabId) {
-  console.log("Get volume");
-  return tabId in tabval ? tabval[tabId] : 1;
+  console.log("///get vol works");
+  return tabId in tabs ? tabs[tabId].gainNode.gain.value : 0;
+}
+
+let tabval = {};
+
+/**
+ * Sets a tab's volume. Captures the tab if it wasn't captured.
+ * @param tabId Tab ID
+ * @param vol Volume. `1` means 100%, `0.5` is 50%, etc
+ */
+async function setTabVolume(tabId, vol) {
+  console.log("in settabvol");
+
+  if (!(tabId in tabs)) {
+    await captureTab(tabId);
+  }
+
+  console.log("in settabvol2", vol);
+
+  if (vol >= 0 && vol <= 7 && tabId in tabs) {
+    tabs[tabId].gainNode.gain.value = vol;
+  }
+
+  console.log("this", tabs[tabId]);
+
+  updateBadge(tabId, vol);
+  tabval[tabId] = vol;
 }
 
 /**
- * Updates the badge to represent current volume.
+ * Updates the badge which represents current volume.
  * @param tabId Tab ID
- * @param volume Volume
+ * @param volume Volume. `1` will display 100, `0.5` - 50, etc
  */
 function updateBadge(tabId, vol) {
   if (tabId in tabs) {
-    const text = String(Math.round(vol * 100));
-    chrome.action.setBadgeText({ text, tabId });
+    const text = String(Math.round(vol * 100)); // I love rounding errors!
+    chrome.browserAction.setBadgeText({ text, tabId });
   }
 }
 
 /**
- * Disposes the tab's resources.
+ * Removes the tab from `tabs` object and closes its AudioContext.
+ * This function gets called when a tab is closed.
  * @param tabId Tab ID
  */
 function disposeTab(tabId) {
   if (tabId in tabs) {
+    tabs[tabId].audioContext.close();
     delete tabs[tabId];
   }
 }
--- popup.js
+++ popup.js
@@ -10,21 +10,15 @@ const setvol = (vol) => {
   voltext.innerText = vol !== null ? `${Math.round(vol * 100)}%` : "--";
 };
 
-
 window.onload = function () {
-  // voltext.innerText = "--";
-  // voltext.innerText = `${100}%`;
-  // console.log("doneeeee")
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     if (tabs.length > 0) {
       tab = tabs[0];
       tabId = tab.id;
-      console.log("ahhhhhhhhhhhhhh");
       chrome.runtime.sendMessage(
         { name: "idk-man", tabId, mute: false },
         (vol) => {
-          console.log("hmmmmmm", vol);
-          if(vol!=undefined){//to set the intial at 100 and not at --
+          if (vol != undefined) {
             slider.value = vol;
             setvol(vol);
           }
@@ -40,16 +34,13 @@ const setMuted = (muted) => {
 
 slider.addEventListener("change", (el) => {
   let newVol = parseFloat(window.event.target.value);
-  console.log("on change newvol: ", newVol);
 
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     if (tabs.length > 0) {
       tab = tabs[0];
       tabId = tab.id;
 
-      console.log(`vol ${newVol} for tabId ${tabId}`);
-      voltext.innerText = `${Math.round(newVol * 100)}%`; // Fixed rounding error decode
-      console.log("mess sent set vol");
+      voltext.innerText = `${Math.round(newVol * 100)}%`;
       chrome.runtime.sendMessage(
         { name: "set-tab-volume", tabId, newVol, mute: false },
         (muted) => {
@@ -58,40 +49,27 @@ slider.addEventListener("change", (el) => {
       );
     }
   });
-  console.log(parseFloat(window.event.target.value),"sssssssssssssssssssssssssssssss")
 });
 
-// mutebtn.addEventListener("click", () => {
-//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//     if (tabs.length > 0) {
-//       tab = tabs[0];
-//       tabId = tab.id;
+mutebtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs.length > 0) {
+      tab = tabs[0];
+      tabId = tab.id;
 
-//       let newVol = parseFloat(slider.value);
-//       if (voltext.innerText === "muted") {
-//         console.log("hhhhhhhhhhhhhhhhhhhhh",lastvol);
-//         chrome.runtime.sendMessage(
-//           { name: "set-tab-volume", tabId, lastvol, mute: !isMuted },
-//           (muted) => {
-//             setMuted(muted);
-//           }
-//         );
-//         voltext.innerText = lastvol;
-//       } else {
-//         lastvol = voltext.innerText;
-//         voltext.innerText = "muted";
-//         chrome.runtime.sendMessage(
-//           { name: "set-tab-volume", tabId, newVol, mute: !isMuted },
-//           (muted) => {
-//             setMuted(muted);
-//           }
-//         );
-//       }
-
-//       console.log(`vol ${newVol} for tabId ${tabId}`);
-
-      
-//     }
-//     console.log("nowwwwwwww",isMuted);
-//   });
-// });
+      chrome.runtime.sendMessage({ name: "toggle-mute", tabId }, (response) => {
+        if (response && response.muted !== undefined) {
+          setMuted(response.muted);
+          if (response.muted) {
+            lastvol = parseFloat(slider.value);
+            slider.value = 0;
+            setvol(0);
+          } else {
+            slider.value = response.volume;
+            setvol(response.volume);
+          }
+        }
+      });
+    }
+  });
+});
